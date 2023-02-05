b97edf75bc6ff48c327c3c44d84eccf1c372b5b0
tacamach <tamara.camacho-rivas@capgemini.com>
2023-02-05 15:33:29 +0100

    add h2 dependency & configuration db schema

--- src/main/java/com/plexus/superheros/SuperherosApplication.java
+++ src/main/java/com/plexus/superheros/SuperherosApplication.java
@@ -8,7 +8,6 @@ public class SuperherosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperherosApplication.class, args);
-		System.out.println("Hola super heroes!!");
 	}
 
 }

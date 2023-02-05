9ba591377dadb5e9aa6e94bdd6138c5755399289
tacamach <tamara.camacho-rivas@capgemini.com>
2023-02-05 16:26:36 +0100

    Implement Service logic

--- src/main/java/com/plexus/superheros/service/impl/SuperherosServiceImpl.java
+++ src/main/java/com/plexus/superheros/service/impl/SuperherosServiceImpl.java
@@ -3,6 +3,7 @@ package com.plexus.superheros.service.impl;
 import com.plexus.superheros.entity.Superhero;
 import com.plexus.superheros.repository.SuperheroRepository;
 import com.plexus.superheros.service.SuperherosService;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
 
@@ -13,21 +14,36 @@ public class SuperherosServiceImpl implements SuperherosService {
 
   @Override
   public Superhero saveHero(Superhero superhero) {
-    return null;
+    return superheroRepository.save(superhero);
   }
 
   @Override
   public Optional<Superhero> findSuperheroById(Long id) {
-    return Optional.empty();
+    return Optional.ofNullable(
+        superheroRepository
+            .findById(id)
+            .orElseThrow(
+                () -> {
+                  throw new EntityNotFoundException();
+                }));
   }
 
   @Override
   public Superhero modifySuperhero(Long id, Superhero superhero) {
-    return null;
+    Superhero modifySuperhero = superheroRepository.findById(id).orElseThrow(
+        EntityNotFoundException::new);
+      modifySuperhero.setName(superhero.getName());
+      modifySuperhero.setSuperpower(superhero.getSuperpower());
+      return superheroRepository.save(modifySuperhero);
   }
 
   @Override
   public boolean deleteSuperhero(Long id) {
-    return false;
+    try {
+      superheroRepository.deleteById(id);
+      return Boolean.TRUE;
+    } catch (Exception e) {
+      return Boolean.FALSE;
+    }
   }
 }
